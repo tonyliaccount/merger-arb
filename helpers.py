a28369cd86ed63295ecb5f1020e47f43022be83c
Adam Grinbergs <adam.grinbergs@gmail.com>
2021-06-20 19:34:14 -0400

    Updating README

--- helpers.py
+++ helpers.py
@@ -67,32 +67,3 @@ def create_connection(db_file):
         return conn
     except Error as e:
         print(e)
-
-
-# def create_table(conn, create_table_sql):
-#     # Does this need to be here?
-#     try:
-#         c = conn.cursor()
-#         c.execute(create_table_sql)
-#     except Error as e:
-#         print(e)
-
-
-# def list_to_db(db, articles: list):
-#     # Does this need to be here?
-#     # Process the list using Spacy
-#     deals = spacy_nlp.name_and_amount(articles)
-#     # Add missing deals into the database
-#     for d in deals:
-#         data_tuple = (d[0], d[1], d[2], d[3])
-#         db.execute("INSERT INTO financings(Datetime, Title, Borrower,"
-#                    + "Amount) VALUES(?,?,?,?);", data_tuple)
-
-
-# conn = create_connection('deals.db')
-# fp = r'C:\Users\Adam\OneDrive\HSA\Resources\JuniorMiningNetwork\Financing'
-# j_list = json_to_list(fp)
-# cur = conn.cursor()
-# list_to_db(cur, j_list)
-# conn.commit()
-# conn.close()
