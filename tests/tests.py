d6b379c9a70785eb71f395bbc0218e58353b5a0b
Adam Grinbergs <adam.grinbergs@gmail.com>
2021-10-12 22:39:43 -0400

    Finished writing arb_calculator, wrote basic test

--- tests/tests.py
+++ tests/tests.py
@@ -1,19 +1,22 @@
 import unittest
-from helpers import calculate_acquisition_arb, margin_call_price
+from helpers import calculate_acquisition_arb, margin_call_price, \
+    calculate_merger_arb
 
-# class CalculateMergerTestCase(unittest.TestCase):
-#     def test_positive(self):
-#         """Check a regular case with a reasonable merger"""
-#         days = 30 * 4 # ~4 months
-#         stocks = [{'Name':'KLG', 'Price':51.43}, {'Name':'AEM',
-#                   'Price':67.29}]
-#         exchange_range = 0.7935
-#         margin_interest = 0.04
-#         commission = 9.99
-#         position_size = 200000
-#         initial_margin = 0.4
-#         assert(calculate_merger(days, stocks, exchange_range,
-#                margin_interest, commission, position_size, initial_margin), )
+
+class CalculateMergerTestCase(unittest.TestCase):
+    def test_positive(self):
+        """Check a regular case with a reasonable merger"""
+        args = {
+            'days': 30 * 4,  # ~4 months
+            'stocks': [{'Name': 'KLG', 'Price': 51.43},
+                       {'Name': 'AEM', 'Price': 67.29}],
+            'exchange_rate': 0.7935,
+            'margin_interest': 0.04,
+            'commission': 9.99,
+            'position_size': 200000,
+            'initial_margin': 0.4,
+        }
+        assert(calculate_merger_arb(**args))  # I need to finish this
 
 
 class AcqArbTestCase(unittest.TestCase):
