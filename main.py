6e940ae08fba7fbcdfc864f343c3746545de4ee6
Tony <tony.li.email@gmail.com>
2021-10-30 13:45:54 -0400

    Heroku setup.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
+#from flask_cors import CORS
 
 # Ariadne is a Python library for implementing GraphQL servers. 
 from ariadne import graphql_sync, make_executable_schema, gql, load_schema_from_path
@@ -17,7 +17,7 @@ app = Flask(__name__)
 # The following allows for cross-origin resource sharing. It's can increase
 # security risks, and should be removed if the frontend is also running on 
 # the same origin.
-CORS(app)
+#CORS(app)
 
 # This is a test route, and can be removed.
 @app.route('/hello', methods=['GET'])
@@ -45,10 +45,3 @@ def graphql_server():
     status_code = 200 if success else 400
     
     return jsonify(result), status_code
-
-
-if __name__ == '__main__':
-    app.run(
-        host='0.0.0.0',
-        debug = True
-    )
